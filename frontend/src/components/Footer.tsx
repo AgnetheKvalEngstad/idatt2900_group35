eee90a1e647b7755cfc483e4742565318d55c175
Maria-Lane <142207454+Maria-Lane@users.noreply.github.com>
2025-03-24 14:19:27 +0100

    feat: Help button shows popover when clicked

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,11 +1,30 @@
-import { Button, Grid2 } from "@mui/material";
+import {
+  Button,
+  Grid2,
+  Dialog,
+  Typography,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import React from "react";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 /**
  * Component for the footer of the page
  * @returns Footer component
  */
 export default function Footer() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <Grid2
       container
@@ -23,9 +42,47 @@ export default function Footer() {
           variant="contained"
           color="primary"
           startIcon={<QuestionMarkIcon />}
+          onClick={handleClickOpen}
         >
           Hjelp
         </Button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle sx={{ m: 0, p: 2 }}>Hjelp</DialogTitle>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={(theme) => ({
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: theme.palette.grey[500],
+            })}
+          >
+            <CloseIcon />
+          </IconButton>
+          <DialogContent dividers sx={{ p: 4 }}>
+            <Typography gutterBottom>
+              Her kan du lære om nettsikkerhet på en morsom måte. Du kan velge
+              hvilke temaer du ønsker å lære mer om ved å trykke på en av
+              temaboksene.
+            </Typography>
+            <Typography gutterBottom>
+              Inne i disse boksene vil du lære om et tema tilknyttet
+              nettsikkerhet. Etter det kan du prøve å vinne premier gjennom å
+              teste hvor mye du har lært.
+            </Typography>
+            <Typography gutterBottom>
+              Dersom noe er utydelig kan du alltid få mer informasjon om siden
+              du er på ved å trykke på “Hjelp” knappen i nedre venstre hjørne.
+            </Typography>
+            <Typography gutterBottom>Lykke til og ha det gøy!</Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleClose}>
+              Lukk
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Grid2>
     </Grid2>
   );
