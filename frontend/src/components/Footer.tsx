4e14aec6d59d2d57e111cd3721a76f34d52233b8
Maria-Lane <142207454+Maria-Lane@users.noreply.github.com>
2025-03-24 14:59:38 +0100

    feat: Added popover for text size info

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,30 +1,25 @@
-import {
-  Button,
-  Grid2,
-  Dialog,
-  Typography,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-} from "@mui/material";
-import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import React from "react";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
+import { Grid2, Button } from "@mui/material";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import TextSizeDialog from "./TextSizeDialog";
+import HelpDialog from "./HelpDialog";
 
 /**
- * Component for the footer of the page
- * @returns Footer component
+ * A React component that renders a footer with buttons for increasing text size
+ * and getting information and instructions for the page.
+ *
+ * @returns A React component with buttons for increasing text size and accessing help.
  */
 export default function Footer() {
-  const [open, setOpen] = React.useState(false);
+  const [openTextSize, setOpenTextSize] = React.useState(false);
+  const [openHelp, setOpenHelp] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const handleTextSizeOpen = () => setOpenTextSize(true);
+  const handleTextSizeClose = () => setOpenTextSize(false);
+
+  const handleHelpOpen = () => setOpenHelp(true);
+  const handleHelpClose = () => setOpenHelp(false);
+
   return (
     <Grid2
       container
@@ -33,56 +28,26 @@ export default function Footer() {
       className="mt-auto flex pl-4 py-2"
     >
       <Grid2>
-        <Button variant="contained" color="primary">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleTextSizeOpen}
+        >
           Stor skrift
         </Button>
+        <TextSizeDialog open={openTextSize} onClose={handleTextSizeClose} />
       </Grid2>
+
       <Grid2>
         <Button
           variant="contained"
           color="primary"
           startIcon={<QuestionMarkIcon />}
-          onClick={handleClickOpen}
+          onClick={handleHelpOpen}
         >
           Hjelp
         </Button>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle sx={{ m: 0, p: 2 }}>Hjelp</DialogTitle>
-          <IconButton
-            aria-label="close"
-            onClick={handleClose}
-            sx={(theme) => ({
-              position: "absolute",
-              right: 8,
-              top: 8,
-              color: theme.palette.grey[500],
-            })}
-          >
-            <CloseIcon />
-          </IconButton>
-          <DialogContent dividers sx={{ p: 4 }}>
-            <Typography gutterBottom>
-              Her kan du lære om nettsikkerhet på en morsom måte. Du kan velge
-              hvilke temaer du ønsker å lære mer om ved å trykke på en av
-              temaboksene.
-            </Typography>
-            <Typography gutterBottom>
-              Inne i disse boksene vil du lære om et tema tilknyttet
-              nettsikkerhet. Etter det kan du prøve å vinne premier gjennom å
-              teste hvor mye du har lært.
-            </Typography>
-            <Typography gutterBottom>
-              Dersom noe er utydelig kan du alltid få mer informasjon om siden
-              du er på ved å trykke på “Hjelp” knappen i nedre venstre hjørne.
-            </Typography>
-            <Typography gutterBottom>Lykke til og ha det gøy!</Typography>
-          </DialogContent>
-          <DialogActions>
-            <Button autoFocus onClick={handleClose}>
-              Lukk
-            </Button>
-          </DialogActions>
-        </Dialog>
+        <HelpDialog open={openHelp} onClose={handleHelpClose} />
       </Grid2>
     </Grid2>
   );
