c62a5674beee53e90ac40575c7e5ae89adc42275
Agnethe Kval-Engstad <agnethek-e@hotmail.com>
2025-05-01 10:02:32 +0200

    feat: Added topic ids to user

--- backend/models/User.cs
+++ backend/models/User.cs
@@ -5,6 +5,7 @@ public class User
     public int Id { get; set; }
     
     public ICollection<Topic> Topics { get; set; } = new List<Topic>();
+    public ICollection<int> TopicIds { get; set; } = new List<int>();
     
     public ICollection<Task> Tasks { get; set; } = new List<Task>();
     
